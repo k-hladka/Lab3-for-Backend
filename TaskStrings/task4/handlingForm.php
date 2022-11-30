9e53316303b1fb3332496b26f3945f346c7efbbe
Kateryna Hladka <pi60_gkv@student.ztu.edu.ua>
2022-11-30 18:32:47 +0200

    Fixed errors

--- TaskStrings/task4/handlingForm.php
+++ TaskStrings/task4/handlingForm.php
@@ -1,18 +1,9 @@
 <?php
 if(isset($_POST['date1']) && isset($_POST['date2'])){
     $timestamp1 =  strtotime($_POST['date1']);
-    if($timestamp1 == false){
-        echo "Помилка введення першої дати!";
-        return;
-    }
-
     $timestamp2 =  strtotime($_POST['date2']);
-    if($timestamp2 == false){
-        echo "Помилка введення другої дати!";
-        return;
-    }
-    $numberDifference = round(($timestamp2 - $timestamp1)/(60*60*24));
-    $numberDifference = abs($numberDifference);
+
+    $numberDifference = abs(round(($timestamp2 - $timestamp1)/(60*60*24)));
     echo "Результат різниці дат: $numberDifference днів";
 }
 
